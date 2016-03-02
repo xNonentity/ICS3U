609fa86db52684b517ba1dbd3f8fc59371bd64a0
Jennifer Nguyen <Jennifernguyen_3233@hotmail.com>
2016-03-02 15:00:32 -0500

    I added more explanation marks and a dot before "Hello"

--- ICS3U/src/nguyen/HelloWorld.java
+++ ICS3U/src/nguyen/HelloWorld.java
@@ -8,7 +8,7 @@ public class HelloWorld {
  */
 	public static void main(String[] args) {
 		//This prints "Hello World" to the console
-		System.out.println("Hello World! (:");
+		System.out.println("•Hello World!!!!");
 
 	}
 
