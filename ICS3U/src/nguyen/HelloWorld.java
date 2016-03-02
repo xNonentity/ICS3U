b8ff5f70ec026cece143d50fc387369688d3c62c
Jennifer Nguyen <Jennifernguyen_3233@hotmail.com>
2016-03-02 14:45:05 -0500

    First Java Program push

---
+++ ICS3U/src/nguyen/HelloWorld.java
@@ -0,0 +1,15 @@
+package nguyen;
+
+public class HelloWorld {
+/* Hello World.Java  
+ * This program prints "Hello World" to the console. 
+ * Jennifer Nguyen 
+ * March 2nd 2015 
+ */
+	public static void main(String[] args) {
+		//This prints "Hello World" to the console
+		System.out.println("Hello World! (:");
+
+	}
+
+}
