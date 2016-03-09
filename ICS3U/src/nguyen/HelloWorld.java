0f513f446a6e7fb1c7d94f3c39848d39901a3094
Jennifer Nguyen <Jennifernguyen_3233@hotmail.com>
2016-03-09 14:40:09 -0500

    This is the parent/age program.

--- ICS3U/src/nguyen/HelloWorld.java
+++ ICS3U/src/nguyen/HelloWorld.java
@@ -1,15 +1,43 @@
 package nguyen;
 
-public class HelloWorld {
-/* Hello World.Java  
- * This program prints "Hello World" to the console. 
- * Jennifer Nguyen 
- * March 2nd 2015 
- */
-	public static void main(String[] args) {
-		//This prints "Hello World" to the console
-		System.out.println("•Hello World!!!!");
+import java.util.Scanner;
 
+public class HelloWorld {
+
+	//Jennifer Nguyen
+	// March 9th 2016 
+
+	
+                  
+
+	public static void main(String[] args) 
+	{
+		
+		Scanner myInput = new Scanner(System.in); 
+		
+		//Variables 
+		int parentsAge; 
+		int childsAge; 
+		int differenceAge; 
+		String mothersName; 
+		String childsName; 
+		
+		System.out.println("What is your name?"); //Parent's Name 
+		mothersName = myInput.nextLine(); 
+		
+		System.out.println("What is your birth year?"); //Parent's age
+		parentsAge = Integer.parseInt(myInput.nextLine()); 
+		
+		System.out.println("What is your child's name?"); //Child's name 
+		childsName = myInput.nextLine(); 
+		
+		System.out.println("What is your child's birth year?"); //Child's age 
+		childsAge = Integer.parseInt(myInput.nextLine()); 
+		
+		//Calculation
+		differenceAge = childsAge = parentsAge; 
+		
+		System.out.println("Hi " + mothersName + ", you were born in " + parentsAge + " and you had " + childsName + " at the age of " + differenceAge + " years old" + ".");
 	}
 
 }
