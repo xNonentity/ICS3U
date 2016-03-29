66d42a5704bcd55f4b0ce7054a2e7198d78e94c9
Jennifer Nguyen <Jennifernguyen_3233@hotmail.com>
2016-03-29 14:43:17 -0400

    Random Dice Program with a While Loop

--- ICS3U/src/nguyen/RandomDiceGenerator.java
+++ ICS3U/src/nguyen/RandomDiceGenerator.java
@@ -6,44 +6,62 @@ public class RandomDiceGenerator {
 
 	public static void main(String[] args) {
 		// Jennifer Nguyen
-		// March 23 2016 
+		// March 23 2016
+
+		Scanner myInput = new Scanner(System.in);
+
+		// Variables
+
+		int dieOne;
+		int dieTwo;
+		int resultUser;
+		int compUser;
+		String userInput;
 		
-		Scanner myInput = new Scanner(System.in); 
-		
-		//Variables 
-		
-		int dieOne; 
-		int dieTwo; 
-		int resultUser; 
-		int compUser; 
-		
-		//Generating for User
-		dieOne = (int)(Math.random() * 6) + 1; 
-		dieTwo = (int)(Math.random() * 6) + 1; 
-		
-		resultUser = dieOne + dieTwo; //Math for result for user 
-		
-		//Generating for computer
-		dieOne = (int)(Math.random() * 6) + 1; 
-		dieTwo = (int)(Math.random() * 6) + 1; 
-		
-		compUser = dieOne + dieTwo; //Math for result for computer
-		
-		//If statement 
-		if (resultUser > compUser){
-			
-			System.out.println("You have won!");
-			System.out.println("Your score: " + resultUser);
-			System.out.println("Computer's score: " + compUser);
+		//Boolean to set a true or false
+		boolean gameEnd = false;
+
+		//While loop 
+		while (gameEnd == false) {
+			// Generating for User
+			dieOne = (int) (Math.random() * 6) + 1;
+			dieTwo = (int) (Math.random() * 6) + 1;
+
+			resultUser = dieOne + dieTwo; // Math for result for user
+
+			// Generating for computer
+			dieOne = (int) (Math.random() * 6) + 1;
+			dieTwo = (int) (Math.random() * 6) + 1;
+
+			compUser = dieOne + dieTwo; // Math for result for computer
+
+			// If statement
+			if (resultUser > compUser) {
+
+				System.out.println("You have won!");
+				System.out.println("Your score: " + resultUser);
+				System.out.println("Computer's score: " + compUser);
+			}
+
+			else {
+				System.out.println("The computer has won.");
+				System.out.println("Your score: " + resultUser);
+				System.out.println("Computer's score: " + compUser);
+
+			}
+
+			System.out.println("Do you want to play again? ('YES' or 'NO')");
+			userInput = myInput.nextLine();
+
+			//If statement determining if the program shall continue
+			if (userInput.equals("YES")) {
+
+				gameEnd = false;
+			} else {
+				gameEnd = true;
+
+			}
 		}
-		
-		else {
-		System.out.println("The computer has won.");
-		System.out.println("Your score: " + resultUser);
-		System.out.println("Computer's score: " + compUser);
-		
-		}
-		
 
 	}
 
