c162dcc23844471ecb5c0a966f8fec51ee1a97b0
Jennifer Nguyen <Jennifernguyen_3233@hotmail.com>
2016-04-26 15:02:16 -0400

    The verses array assignment that holds 10 actions in the array from Some Practice Problems (b)

---
+++ ICS3U/src/nguyen/VersesArraryAssignment.java
@@ -0,0 +1,27 @@
+package nguyen;
+/* Jennifer Nguyen
+ *  April 26 2016
+ *  Some Practice Problems (B)
+ *  Song versus with two actions in the array
+ */
+
+public class VersesArraryAssignment {
+
+	public static void main(String[] args) {
+		
+		
+		String[] versus = new String[]{" clap your hands."," stomp your feet.", " shake your hands.", " laugh out loud.", " dance it out." , " nod your head.", " wiggle your arms.", " jump really high.", " hug your friends.", " pat your head." };
+		
+		
+		for (int i = 0; i < 10; i++){
+			
+			System.out.println("If you're happy and you know it" + versus[i]);
+			System.out.println("If you're happy and you know it" + versus[i]);
+			System.out.println("If you're happy and you know it and you really want to show it.");
+			System.out.println("If you're happy and you know it" + versus[i]);
+			
+		}
+
+	}
+
+}
