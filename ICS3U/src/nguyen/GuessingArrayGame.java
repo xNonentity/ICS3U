53e6781c154a34aead4a657f4d51e463c0c04ed3
Jennifer Nguyen <Jennifernguyen_3233@hotmail.com>
2016-04-29 15:12:24 -0400

    Guessing Game Array, think of a type of dog and the computer guesses!

--- ICS3U/src/nguyen/GuessingArrayGame.java
+++ ICS3U/src/nguyen/GuessingArrayGame.java
@@ -1,4 +1,10 @@
 package nguyen;
+/*
+ * Jennifer Nguyen
+ * April 28 2016
+ * Guessing Game Array 
+ * You think of a type of dog and the computer guesses! 
+ */
 
 import java.util.Scanner;
 
@@ -7,39 +13,55 @@ public class GuessingArrayGame {
 	public static void main(String[] args) {
 
 		Scanner input = new Scanner(System.in);
-
-		String answer;
-		int guesses = 0;
-
+		
 		String[] randItems = new String[] { "Husky", "Poodle", "German Sheperd", "Corgie", "Beagle", "Shih Ztu",
 				"Chow Chow", "Pit Bull", "American Eskimo", "Collie", "Dalmatian", "Greyhound", "Bulldog",
 				"Golden Retriever", "Yorkshire", "Pug", "Chihuahua", "Pomeranian", "Akita", "Shiba", "Boxer",
 				"Great Dane", "Maltese", "Pointer" }; //24
+		String answer;
+		int guesses = 0;
+		int i = 0;
+		boolean [] guess = new boolean [randItems.length];
+		
 
-		System.out.println("Hi, let's play a guessing. Think of a type of dog and enter any key to play.");
-		input.nextLine();
-
-		for (int i = 23; i != -1; i--) {
-
-			guesses++;
-
-			int randNum = (int) (Math.random() * 24);
-			System.out.println("Is it a " + randItems[randNum]);
+		System.out.println("Hi, let's play a guessing. Think of a type of dog and enter any key to start. ");
+		answer=input.nextLine();
+		do {
 			
-			answer = input.nextLine();
-
+			
+			int randNum = (int) (Math.random() * randItems.length);
+			//System.out.println("Is it a " + randItems[randNum]);
+			//answer = input.nextLine();
+			
+			
+			if (!guess[randNum]){ 
+				
+				guess[randNum] = true; 
+				System.out.println("Is it a " + randItems[randNum]);
+				guesses++;
+				answer = input.nextLine();
+				i++; 
+			}
 			if (answer.equalsIgnoreCase("Yes")) {
 				System.out.println("I win!");
 				System.out.println("I got it in " + guesses + " guesses!");
 				i = 0;
+			} 
+			
+			if (i == randItems.length) {
+				System.out.println("Darn, I made " + guesses + " guesses..");
+				System.out.println("I don't know anymore, you win.");
+				answer = "yes"; 
+			
+			
 			}
-
+			
 		}
+		while (answer.equalsIgnoreCase("No"));
+		
 
-		if (guesses == 24) {
-			System.out.println("Darn, I made " + guesses + " guesses..");
-			System.out.println("I don't know anymore, you win.");
-		}
+		
+		
 
 	}
 
