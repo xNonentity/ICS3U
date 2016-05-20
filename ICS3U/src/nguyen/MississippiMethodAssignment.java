46ede34d0a2bb474cc2f7dc58d4b386ad6b092a1
Jennifer Nguyen <Jennifernguyen_3233@hotmail.com>
2016-05-20 14:02:04 -0400

    This program will print out MISSISSIPPI by using methods

---
+++ ICS3U/src/nguyen/MississippiMethodAssignment.java
@@ -0,0 +1,118 @@
+package nguyen;
+/*
+ * Jennifer Nguyen
+ *  May 18th 2016 
+ *  This program spells out MISSISSIPPI by using methods. 
+ * 
+ */
+public class MississippiMethodAssignment {
+
+public class commenting {
+		
+		/** 
+		 * The  method calls out the other methods to spell out MISSISSIPPI.
+		 * 
+		 */
+		
+	}
+	public static void main(String[] args) {
+
+		drawM(); 
+		drawI(); 
+		drawS();
+		drawS(); 
+		drawI(); 
+		drawS();
+		drawS(); 
+		drawI(); 
+		drawP();
+		drawP(); 
+		drawI();
+	} 
+
+public class commentingM {
+		
+		/** 
+		 *  The method prints out the letter M. 
+		 * 
+		 */
+		
+	}
+	
+	public static void drawM() {
+		
+		System.out.println("M       M");
+		System.out.println("MM     MM");
+		System.out.println("M M   M M");
+		System.out.println("M  M M  M");
+		System.out.println("M   M   M");
+		System.out.println("M       M");
+		
+	} 
+	
+
+public class commentingI {
+		
+		/** 
+		 * The method prints out the letter I.
+		 * 
+		 */
+		
+	}
+	
+	public static void drawI() {
+		
+		System.out.println("IIIIIII");
+		System.out.println("   I   ");
+		System.out.println("   I   ");
+		System.out.println("   I   ");
+		System.out.println("   I   ");
+		System.out.println("   I   ");
+		System.out.println("IIIIIII");
+		
+		
+	} 
+	
+
+public class commentingS {
+		
+		/** 
+		 *  The method prints out S. 
+		 * 
+		 */
+		
+	}
+	
+	public static void drawS() {
+		
+		System.out.println(" SSSSS ");
+		System.out.println("S     S");
+		System.out.println("S");
+		System.out.println(" SSSSS ");
+		System.out.println("      S");
+		System.out.println("S     S");
+		System.out.println(" SSSSS");
+	}
+	
+
+public class commentingP {
+		
+		/** 
+		 * The method prints out the letter P. 
+		 * 
+		 */
+		
+	}
+	
+	public static void drawP() {
+		
+		System.out.println(" PPPPP");
+		System.out.println("P     P");
+		System.out.println("P     P");
+		System.out.println(" PPPPP ");
+		System.out.println("P");
+		System.out.println("P");
+		System.out.println("P");
+	}
+
+}
